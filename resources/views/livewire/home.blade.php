8797a6f1bc024bf4ae398f8834e595898859733c
ndemafiawilsmith <wilsmithndemafia@gmail.com>
2025-10-15 09:42:56 +0100

    Fixed home page

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,9 @@
+<div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <a href="{{ url('/admin/login') }}" class="block">
+        <img
+            src="{{ asset('FloodVerse.png') }}"
+            alt="FloodVerse Landing Page"
+            class="w-full max-w-6xl mx-auto rounded-lg shadow-lg hover:opacity-90 transition-opacity duration-300 cursor-pointer"
+        >
+    </a>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', Home::class)->name('home');
